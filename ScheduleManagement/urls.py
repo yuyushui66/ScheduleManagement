53f534a42c839f01cef67788dffabd9ae2c9f535
Nitera <108538898+Nitera@users.noreply.github.com>
2023-04-08 20:32:03 +0800

    add signup url

--- ScheduleManagement/urls.py
+++ ScheduleManagement/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls. static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login),
+    path('signup/', views.signup),
     path('index/', views.index),
     path('state/', views.state),
 
