b04ecbaff43e10e66b992db3956613a3d84144e1
lilililililimiles <3438814875@qq.com>
2023-05-03 12:52:47 +0800

    前端日程数据获取，存放于ScheduleView.schedule

--- register/urls.py
+++ register/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloApi/', views.HelloView.as_view()),
+    path('scheduleApi/', views.ScheduleView.as_view()),
 ]
 
--- register/views.py
+++ register/views.py
@@ -4,19 +4,22 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-test = []
-class HelloView(APIView):
+class ScheduleView(APIView):
+    schedule = []
+    # 日程信息，每个元素为[{'title':},{'id':},{'start':},{'end':},{'allday':}]，value均为string
+    # 如果allday='true',则end='null'
+    # [{'title': 'All Day Event'}, {'id': '_fc1'}, {'start': 'Mon May 01 2023 00:00:00 GMT+0800 (中国标准时间)'}, {'end': 'null'}, {'allday': 'true'}]
     def get(self, request, *args, **kwargs):
-        # title=request.query_params.dict()['title'].replace('\n','')
-        # start=request.query_params.dict()['start'].replace('\n','')
-        # end=request.query_params.dict()['end'].replace('\n','')
-        # allday=request.query_params.dict()['allday'].replace('\n','')
-        n=request.query_params
+        n=request.query_params.dict()
+        for i in range(int(len(n)/5)):
+            if [{'title':n['data['+str(i)+'][0][]']},{'id':n['data['+str(i)+'][1][]']},
+                                {'start':n['data['+str(i)+'][2][0]']},{'end':n['data['+str(i)+'][3][0]']},
+                                {'allday':n['data['+str(i)+'][4][]']}] not in self.schedule:# 判断重复
+                self.schedule.append([{'title':n['data['+str(i)+'][0][]']},{'id':n['data['+str(i)+'][1][]']},
+                                    {'start':n['data['+str(i)+'][2][0]']},{'end':n['data['+str(i)+'][3][0]']},
+                                    {'allday':n['data['+str(i)+'][4][]']}])
+        for i in range(len(self.schedule)):
+            print(self.schedule[i])
 
-        if n['title'] not in test :
-            test.append(n['title'])
-
-        print(test)
-        # print(title,'\n',start,'\n',end,'\n',allday)
         return Response({"status": True, 'message': '发送成功'})
 
